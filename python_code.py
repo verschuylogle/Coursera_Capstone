190004abd648d0790f27aa915af0ea5e4fdbdc6e
Marge Ogle <60993676+verschuylogle@users.noreply.github.com>
2020-02-12 21:13:52 -0600

    Update python_code.py
    delete code that was duplicated by mistake

--- python_code.py
+++ python_code.py
@@ -4,8 +4,4 @@
 import pandas as pd
 import numpy as np
 print("Hello Capstone Project Course!"#Coursera Capstone Project from Marge Ogle
-        # 
-        # Import required Libraries
-        import pandas as pd
-        import numpy as np
-        print("Hello Capstone Project Course")
+       
