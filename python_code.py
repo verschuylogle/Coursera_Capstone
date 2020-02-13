546930af36f96c1a31e11d8d14f3f98326eb6de5
Marge Ogle <marge@oglemail.org>
2020-02-12 20:53:40 -0600

    Initial Commit of Python required libraries and greeting

---
+++ python_code.py
@@ -0,0 +1,11 @@
+#Coursera Capstone Project from Marge Ogle
+# 
+# Import required Libraries
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!"#Coursera Capstone Project from Marge Ogle
+        # 
+        # Import required Libraries
+        import pandas as pd
+        import numpy as np
+        print("Hello Capstone Project Course")
